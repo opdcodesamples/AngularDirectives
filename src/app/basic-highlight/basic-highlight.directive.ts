f585fd7b2153bcce1aad00e4d32dda226aec0fae
Osmand Das <osmandpdas@gmail.com>
2018-09-29 12:23:54 -0400

    updated code

--- src/app/basic-highlight/basic-highlight.directive.ts
+++ src/app/basic-highlight/basic-highlight.directive.ts
@@ -4,12 +4,14 @@ import { Directive, ElementRef, OnInit } from '@angular/core'
     selector: '[appBasicHighlight]'
 })
 export class BasicHighlightDirective implements OnInit{
-    
+    // reference to the element to which the directive is placed on.
     constructor(private elementRef: ElementRef) {
         
     }
 
     ngOnInit() {
+        // accesssing elements directly like this is not a very good practive thorugh
+        // this is only for demo purpose.
         this.elementRef.nativeElement.style.backgroundColor = 'green'
     }
 }
--- src/app/basic-highlight/hostbinding-highlight.directive.ts
+++ src/app/basic-highlight/hostbinding-highlight.directive.ts
@@ -5,8 +5,16 @@ import { Directive, OnInit, HostBinding, HostListener, Input } from '@angular/co
 })
 export class HostBindingHighlightDirective implements OnInit{
 
+    // custom property binding;
     @Input() defaultColor: string = 'transparent'
     @Input() highlightColor: string = 'grey'
+
+    // we can assign an alias for the directive input property which can we directive's selector name
+    // in this case we can wrap the directive in a [] like [appHostBindingHighlight] on the UI element 
+    // and can directly assign the value of input property which we would have assigned to the input
+    // property name within the []
+    
+    // @Input('appHostBindingHighlight') defaultColor: string = 'transparent'
 
     @HostBinding('style.backgroundColor') backgroundColor: string = this.highlightColor;
    
